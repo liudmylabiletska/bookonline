b50b6513e8046a2597201982c98366bcde0d4f73
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-10-19 22:27:02 +0200

    Create CI.yml modified BookMapper.java

--- src/main/java/mate/academy/bookonline/mapper/BookMapper.java
+++ src/main/java/mate/academy/bookonline/mapper/BookMapper.java
@@ -16,7 +16,7 @@ public interface BookMapper {
 
     BookDto toDto(Book book);
 
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
     Book toModel(CreateBookRequestDto createBookRequestDto);
 
     @Mapping(target = "id", ignore = true)
